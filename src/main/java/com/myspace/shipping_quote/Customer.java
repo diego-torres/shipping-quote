023d15d52290a059420e33fac440d10b9fa1d390
jboss <>
2019-01-17 10:00:15 -0600

    {/src/main/java/com/myspace/shipping_quote/Customer.java}

--- src/main/java/com/myspace/shipping_quote/Customer.java
+++ src/main/java/com/myspace/shipping_quote/Customer.java
@@ -6,12 +6,33 @@ package com.myspace.shipping_quote;
 
 public class Customer implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Customer() {
-    }
+	private java.lang.Integer id;
+	private java.lang.String firstName;
 
+	public Customer() {
+	}
 
+	public java.lang.Integer getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(java.lang.String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Customer(java.lang.Integer id, java.lang.String firstName) {
+		this.id = id;
+		this.firstName = firstName;
+	}
 
 }
